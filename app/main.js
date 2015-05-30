a9de215bab4ab9bfc5295638fd447f57d6d22b0e
kigiri <le.mikmac@gmail.com>
2015-05-30 04:05:46 +0200

    clean up main

--- app/main.js
+++ app/main.js
@@ -1,25 +1,6 @@
-/* global App, $state */
+/* global App, $add */
 
-new App();
+$add((new App()).HTMLElement);
 
-// var ptest =  $add(new Page("img/heavy.png", 0).load());
-window.onscroll = function (event) {
-  $state.setScroll((event.pageY !== undefined)
-    ? event.pageY
-    : document.body.scrollTop);
-};
-
-window.onmouseup = $state.mouseRelease;
-
-window.onmousemove = function (event) {
-  $state.setMouse(event);
-};
-
-window.addEventListener("orientationchange", $state.updateWindow, false);
-window.addEventListener("resize", $state.updateWindow, false);
-// window.addEventListener("touchstart", $state.touchStart, false);
-// window.addEventListener("touchend", $state.touchEnd, false);
-// window.addEventListener("touchleave", $state.touchEnd, false);
-// window.addEventListener("touchcancel", $state.touchEnd, false);
 function $main() {}
 

8b03f066ee80162d03e62e4af1bc432a14cf7295
kigiri <le.mikmac@gmail.com>
2015-05-30 04:05:32 +0200

    some love for libraries too

--- app/library/ez.js
+++ app/library/ez.js
@@ -1,1 +1,40 @@
-function $ez() {};
+var $ez = {
+  // Sorts generator
+  by: function (key, reverse) {
+    reverse = reverse ? -1 : 1;
+    return function (a, b) {
+      a = a[key];
+      b = b[key];
+      if (a === b) { return 0; }
+      if (a > b) { return reverse; }
+      return -reverse;
+    }
+  },
+
+  // Filters
+  all: function (key, value) {
+    return function (a) {
+      return a[key] === value;
+    }
+  },
+  none: function (key, value) {
+    return function (a) {
+      return a[key] !== value;
+    }
+  },
+  removeEmpty: function (e) {
+    return !(!e || !e.length);
+  },
+
+  // Shims
+  now: (function () {
+    return window.performance
+      ? window.performance.now.bind(window.performance)
+      : Date.now.bind(Date);
+  })()
+};
+
+// pre-builded sorts :
+$ez.byScore = $ez.by("score");
+$ez.byIndex = $ez.by("index");
+$ez.byPriority = $ez.by("priority");
